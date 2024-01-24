06a3842be8c6206882e27f81ce5274d74d10dd7e
Massimo Cappellano <massimo.cappe@gmail.com>
2024-01-24 10:45:35 +0100

    Initial import

---
+++ README.md
@@ -0,0 +1,7 @@
+# Esempio Produttore-Consumatore con scambio messaggi
+
+ProducerConsumer. Provare ad eseguire il programma multithread Factory.java nella cartella ProducerConsumer
+che implementa in Java una soluzione al problema del produttore-consumatore tramite scambio di messaggi. Cosa
+succede? Funziona correttamente? Perché non richiede sincronizzazione dei thread?
+[Suggerimento: Vedere dalla documentazione Java, la definizione della classe Vector usata per implementare il
+canale di comunicazione (oggetto condiviso).]
---
+++ src/main/java/ProducerConsumer/Channel.java
@@ -0,0 +1,23 @@
+package ProducerConsumer;
+
+/**
+ * An interface for a message passing scheme.
+ *
+ * @author Gagne, Galvin, Silberschatz
+ * Operating System Concepts with Java - Sixth Edition
+ * Copyright John Wiley & Sons - 2003.
+ */
+
+public interface Channel {
+    /**
+     * Send a message to the channel.
+     * It is possible that this method may or may not block.
+     */
+    public abstract void send(Object message);
+
+    /**
+     * Receive a message from the channel
+     * It is possible that this method may or may not block.
+     */
+    public abstract Object receive();
+}
